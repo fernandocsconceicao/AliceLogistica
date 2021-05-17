3d99a8ce24171c7cc11f9f752419b8f7c9cfd9c3
Fernando Cesar <fernando.csconceicao@outlook.com>
2021-05-17 11:08:46 -0300

    Commit inicial

---
+++ src/main/kotlin/com/alicelogistica/AliceLogistica/AliceLogisticaApplication.kt
@@ -0,0 +1,11 @@
+package com.alicelogistica.AliceLogistica
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AliceLogisticaApplication
+
+fun main(args: Array<String>) {
+	runApplication<AliceLogisticaApplication>(*args)
+}
---
+++ src/test/kotlin/com/alicelogistica/AliceLogistica/AliceLogisticaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alicelogistica.AliceLogistica
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AliceLogisticaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
